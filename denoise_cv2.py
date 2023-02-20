4c61721e38807ef2d43a0335f10b75b46747ffaa
mhmdalkhyyt <mhmdalkhyyt@gmail.com>
2023-02-20 18:10:16 +0100

    ss

--- denoise_cv2.py
+++ denoise_cv2.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 #blur pictures
-clear_path = "C:\\Users\\TheGreatOne\\Examens_arbetet\\Yolo_test\\YOLOv8_working\\clear_drone\\*"
+clear_path = "./clear_imgs/*"
 clear_files = glob.glob(clear_path)
 size_of_clear_files = len(clear_files)
 i = 0
@@ -17,14 +17,11 @@ while(i < size_of_clear_files):
 
 
 #run obj detection on blurred images
-blurred_path = "C:\\Users\\TheGreatOne\\Examens_arbetet\\Yolo_test\\YOLOv8_working\\lurred_imgs\\*"
+blurred_path = "./lurred_imgs/*"
 blurred_files = glob.glob(blurred_path)
 size_of_blurred_files = len(blurred_files)
 i = 0
 while(i < size_of_blurred_files):
     subprocess.run(f"python ./obj_detect.py {blurred_files[i]}")
     i = i+1
-
-
-
 
--- main.py
+++ main.py
@@ -5,25 +5,25 @@ import numpy as np
 
 
 #blur pictures
-clear_path = "C:\\Users\\TheGreatOne\\Examens_arbetet\\Yolo_test\\YOLOv8_working\\clear_drone\\*"
+clear_path = "./clear_drone/*"
 clear_files = glob.glob(clear_path)
 size_of_clear_files = len(clear_files)
 i = 0
 while(i < size_of_clear_files):
     subprocess.run(f"python ./blur/python_cli_image_editor/editor.py filter --input {clear_files[i]} --output ./lurred_imgs --blur 4")
-
+    print(f"bluring image {clear_files[i]}")
     i = i+1
+    
 
 
 #run obj detection on blurred images
-blurred_path = "C:\\Users\\TheGreatOne\\Examens_arbetet\\Yolo_test\\YOLOv8_working\\lurred_imgs\\*"
+blurred_path = "./lurred_imgs/*"
 blurred_files = glob.glob(blurred_path)
 size_of_blurred_files = len(blurred_files)
 i = 0
 while(i < size_of_blurred_files):
     subprocess.run(f"python ./obj_detect.py {blurred_files[i]}")
+    print(f"running debluring on blur image {blurred_files[i]}")
     i = i+1
 
-
-
-
+print("done")
